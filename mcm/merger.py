ae4db1b763d1ec06a20213a4c092baf2ff960d5a
Gavin McQuillan <gavin.mcquillan@gmail.com>
2013-12-31 16:12:36 -0800

    Removing unused merger.py

--- mcm/merger.py
+++
@@ -1,7 +0,0 @@
-"""Saving/Upserting data into a database.
-
-While most of this library is meant to be datastore/framework
-agnostic -- this part is where the rubber meets the road.
-
-We have to to play with the [Django] ORM here.
-"""
