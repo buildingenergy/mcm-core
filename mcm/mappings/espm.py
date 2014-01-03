c729d0d6cd27ed785658e89c4d12211cdf9713d4
Gavin McQuillan <gavin.mcquillan@gmail.com>
2014-01-03 11:01:17 -0800

    Add units to ESPM mapping to match real headers

--- mcm/mappings/espm.py
+++ mcm/mappings/espm.py
@@ -12,6 +12,7 @@ go into the ``extra_data`` attribute, which is a json field in Postgres.
 MAP = {
     # Could there be a better key for this?
     u'Property Id': u'property_id',
+    u'Property Name': u'property_name',
     u'Address 1': u'address_line_1',
     u'Address 2': u'address_line_2',
     u'City': u'city',
@@ -19,14 +20,13 @@ MAP = {
     u'Country': u'country',
     u'City': u'city',
     u'Postal Code': u'postal_code',
-    # Not sure what to do about this, no `Organization` field exists.
-    #u'Organization': '?'
+    u'Organization': u'organization',
     u'State/Province': u'state_province',
-    u'Property Floor Area (Buildngs and Parking)': (
+    u'Property Floor Area (Buildings and Parking) (ft2)': (
         u'property_floor_area_bldg_park'
     ),
     u'Property Floor Area (Building(s))': u'property_floor_area_bldg',
-    u'Property Floor Area (Parking)': u'property_floor_area_park',
+    u'Parking - Gross Floor Area (ft2)': u'property_floor_area_park',
     u'Year Built': u'year_built',
     u'Year Ending': u'year_ending',
     u'Energy Alerts': u'energy_alerts',
@@ -34,15 +34,15 @@ MAP = {
     u'ENERGY STAR Certification - Year(s) Certified': u'energy_star_cert_years',
     u'National Median ENERGY STAR Score': u'national_median_energy_star_score',
     u'National Median Reference Property Type': u'national_median_ref_prop_type',
-    u'National Median Site EUI': u'national_median_site_eui',
-    u'National Median Source EUI': u'national_median_source_eui',
+    u'National Median Site EUI (kBtu/ft2)': u'national_median_site_eui',
+    u'National Median Source EUI (kBtu/ft2)': u'national_median_source_eui',
     u'National Median Source Energy Use': u'national_median_site_energy_use',
-    u'Site EUI': u'site_eui',
+    u'Site EUI (kBtu/ft2)': u'site_eui',
     # Might we want `Site Energy Use` at some point?
-    u'Source EUI': u'source_eui',
-    u'Total GHG Emissions': u'total_ghg_emissions',
-    u'Weather Normalized Site EUI': u'weather_normalized_site_eui',
-    u'Weather Normalized Source EUI': u'weather_normalized_source_eui',
+    u'Source EUI (kBtu/ft2)': u'source_eui',
+    u'Total GHG Emissions (MtCO2e)': u'total_ghg_emissions',
+    u'Weather Normalized Site EUI (kBtu/ft2)': u'weather_normalized_site_eui',
+    u'Weather Normalized Source EUI (kBtu/ft2)': u'weather_normalized_source_eui',
     u'Generation Date': u'generation_date',
     u'Release Date': u'release_date',
 }
