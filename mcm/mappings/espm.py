09bb4b02b79ad121725100c5c2a3a04461585c39
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2014-03-28 10:39:39 -0700

    should fix most mapping issues where, mapping are no longer saved to the seed.models BuildingSnapahot fields. [#68416848]

--- mcm/mappings/espm.py
+++ mcm/mappings/espm.py
@@ -16,33 +16,24 @@ MAP = {
     u'Address 1': u'address_line_1',
     u'Address 2': u'address_line_2',
     u'City': u'city',
-    u'County': u'county',
-    u'Country': u'country',
+    u'County': u'district',
     u'City': u'city',
     u'Custom Property ID 1 - ID': u'custom_id_1',
     u'Postal Code': u'postal_code',
     u'State/Province': u'state_province',
     u'Property Floor Area (Buildings and Parking) (ft2)': (
-        u'property_floor_area_bldg_park'
+        u'gross_floor_area'
     ),
-    u'Property Floor Area (Building(s))': u'property_floor_area_bldg',
-    u'Parking - Gross Floor Area (ft2)': u'property_floor_area_park',
     u'Year Built': u'year_built',
     u'Year Ending': u'year_ending',
     u'Energy Alerts': u'energy_alerts',
     u'ENERGY STAR Score': u'energy_score',
-    u'ENERGY STAR Certification - Year(s) Certified': u'energy_star_cert_years',
-    u'National Median ENERGY STAR Score': u'national_median_energy_star_score',
-    u'National Median Reference Property Type': u'national_median_ref_prop_type',
-    u'National Median Site EUI (kBtu/ft2)': u'national_median_site_eui',
-    u'National Median Source EUI (kBtu/ft2)': u'national_median_source_eui',
-    u'National Median Source Energy Use': u'national_median_site_energy_use',
     u'Site EUI (kBtu/ft2)': u'site_eui',
-    # Might we want `Site Energy Use` at some point?
     u'Source EUI (kBtu/ft2)': u'source_eui',
-    u'Total GHG Emissions (MtCO2e)': u'total_ghg_emissions',
-    u'Weather Normalized Site EUI (kBtu/ft2)': u'weather_normalized_site_eui',
-    u'Weather Normalized Source EUI (kBtu/ft2)': u'weather_normalized_source_eui',
+    u'Weather Normalized Site EUI (kBtu/ft2)': u'site_eui_weather_normalized',
+    u'Weather Normalized Source EUI (kBtu/ft2)': (
+        u'source_eui_weather_normalized'
+    ),
     u'Generation Date': u'generation_date',
     u'Release Date': u'release_date',
 }
